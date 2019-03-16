f69181b3ca48eec23577f0e44dd85737ff856a4b
Leandro Lima <leandro.stlima@gmail.com>
2019-03-16 19:49:40 -0300

    Testing factory methods pattern

---
+++ effectivejava/chapter2/src/main/java/chapter2/factorymethod/Employee.java
@@ -0,0 +1,22 @@
+package chapter2.factorymethod;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+
+
+public @Data @AllArgsConstructor(access = AccessLevel.PRIVATE) class Employee {
+    
+    private Long id;
+    private String name;
+    private Float salary;
+    private Float hourRate;
+
+    public static Employee createEmployeeWithSalary(final Long id, final String name, final Float salary) {
+        return new Employee(id, name, salary, null);
+    }
+    
+    public static Employee createEmplyeeWithHourRate(final Long id, final String name, final Float hourRate) {
+        return new Employee(id, name, null, hourRate);
+    }
+}
