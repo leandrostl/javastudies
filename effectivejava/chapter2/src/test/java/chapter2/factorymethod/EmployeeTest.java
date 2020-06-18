e396a43e62ea6784490170e4a5d222f01e570ec4
Leandro Lima <leandro.stlima@gmail.com>
2020-06-17 22:04:26 -0300

    Uso do junit5

---
+++ effectivejava/chapter2/src/test/java/chapter2/factorymethod/EmployeeTest.java
@@ -0,0 +1,26 @@
+package chapter2.factorymethod;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+    final Employee johnEmployee = Employee.createEmployeeWithSalary(1L, "John", 2000.00f);
+    final Employee markEmployee = Employee.createEmplyeeWithHourRate(2L, "Mark", 20.00f);
+
+    @Test
+    @DisplayName("An employee with salary shouldn't have hour rate.")
+    void employeeMonthlyPayed() {
+        assertNull(johnEmployee.getHourRate());
+    }
+
+    @Test
+    @DisplayName("An employee hourly payed shouldn't have salary.")
+    void employeeHourlyPayed() {
+        assertNull(markEmployee.getSalary());
+    }
+
+}
